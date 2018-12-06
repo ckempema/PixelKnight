4f438b5b5b9cd4017f066df85badddf71fc2cb86
ckempema <calderkempema@gmail.com>
2018-12-06 17:08:23 -0500

    Added heroku link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://afternoon-earth-99855.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
